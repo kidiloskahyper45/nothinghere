e211f9416f48e612a3472e36c7fc20e4f61862a9
maskedcoder <85559138+maskedcoder7@users.noreply.github.com>
2021-06-15 15:23:53 +0530

    Update global_bans_sql.py

--- YoneRobot/modules/sql/global_bans_sql.py
+++ YoneRobot/modules/sql/global_bans_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, UnicodeText, Integer, String, Boolean
 
-from YoneRobot.modules.sql import BASE, SESSION
+from cinderella.modules.sql import BASE, SESSION
 
 
 class GloballyBannedUsers(BASE):
