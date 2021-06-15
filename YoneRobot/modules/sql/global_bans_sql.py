778a43579ecd1ff784027e2fa778f45d2637d8f5
theshubhamgundu <80262881+theshubhamgundu@users.noreply.github.com>
2021-06-15 08:24:32 +0530

    Update global_bans_sql.py

--- YoneRobot/modules/sql/global_bans_sql.py
+++ YoneRobot/modules/sql/global_bans_sql.py
@@ -1,7 +1,8 @@
 import threading
 
+from sqlalchemy import Column, UnicodeText, Integer, String, Boolean
+
 from YoneRobot.modules.sql import BASE, SESSION
-from sqlalchemy import Boolean, Column, Integer, String, UnicodeText
 
 
 class GloballyBannedUsers(BASE):
@@ -19,7 +20,9 @@ class GloballyBannedUsers(BASE):
         return "<GBanned User {} ({})>".format(self.name, self.user_id)
 
     def to_dict(self):
-        return {"user_id": self.user_id, "name": self.name, "reason": self.reason}
+        return {"user_id": self.user_id,
+                "name": self.name,
+                "reason": self.reason}
 
 
 class GbanSettings(BASE):
@@ -144,9 +147,7 @@ def __load_gbanned_userid_list():
 def __load_gban_stat_list():
     global GBANSTAT_LIST
     try:
-        GBANSTAT_LIST = {
-            x.chat_id for x in SESSION.query(GbanSettings).all() if not x.setting
-        }
+        GBANSTAT_LIST = {x.chat_id for x in SESSION.query(GbanSettings).all() if not x.setting}
     finally:
         SESSION.close()
 
